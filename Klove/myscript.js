b77731488416e8223d825caeb707a3e38867b924
Kiko Golden <94949765+KikoGolden@users.noreply.github.com>
2021-12-10 15:16:10 +0200

    Add files via upload

---
+++ Klove/myscript.js
@@ -0,0 +1,12 @@
+menuList.style.maxHeight = '0px';
+
+function togglemenu(){
+
+   if(menuList.style.maxHeight == '0px'){
+       menuList.style.maxHeight = '780px';
+   }
+   else{
+       menuList.style.maxHeight = '0px';
+   }
+
+}
